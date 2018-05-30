ff6a8f7a111d3990b966f234a9f3268d2f5abe14
Randall Wilk <wilk.randall@gmail.com>
2018-05-30 12:39:07 -0500

    formatting

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -3,12 +3,12 @@ const { mix } = require('laravel-mix');
 const inProduction = mix.inProduction();
 
 mix
-	.setPublicPath('dist')
-	.js('src/index.js', 'vue-context.js')
-	.sourceMaps(! inProduction)
-	.webpackConfig({
-		output: {
-			libraryTarget: 'umd',
-			umdNamedDefine: true
-		}
-	});
+    .setPublicPath('dist')
+    .js('src/index.js', 'vue-context.js')
+    .sourceMaps(! inProduction)
+    .webpackConfig({
+        output: {
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        }
+    });
