3057cf3f2c37b91e06b68f539d4e18a25e02e71c
Randall Wilk <wilk.randall@gmail.com>
2019-04-03 19:46:36 -0500

    Remove old tests

--- build/webpack-test.mix.js
+++ build/webpack-test.mix.js
@@ -3,5 +3,4 @@ const { mix } = require('laravel-mix');
 mix
     .setPublicPath('test/js/dist')
     .js('test/js/src/index.js', 'index.js')
-    .js('test/js/src/test_issue_4.js', 'test_issue_4.js')
     .sourceMaps();
